69ae5e066a2c4b7be027424f971a54b56d09cc07
nhamtung <nhamtung125@gmail.com>
2020-04-04 11:12:57 +0700

    fix Interrupt2

--- Interrupt/Interrupt2/Interrupt2.ino
+++ Interrupt/Interrupt2/Interrupt2.ino
@@ -1,11 +1,19 @@
-
+/*
+ * interruptModePin on Arduino Nano is pin D2 and D3
+ * Connect button to GND pin and interruptModePin
+ */
+ 
 const byte interruptModePin = 3;
 const byte switchMode = 5;
+const byte ledPin = 13;
+const int timeWaitButtonMode = 500;
+
 int countINT = 0;
 volatile byte stateMode = HIGH;
 
 void setup() {
   Serial.begin(115200);
+  pinMode(switchMode, OUTPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(interruptModePin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptModePin), buttonClick, FALLING);
@@ -16,12 +24,15 @@ void setup() {
 void loop() {
   if(countINT >= 1) {
     stateMode = !stateMode;
+    Serial.println("Change Mode!");
   }
   digitalWrite(switchMode, stateMode);
+  digitalWrite(ledPin, stateMode);
   countINT = 0;
-  delay(1000);
+  delay(timeWaitButtonMode);
 }
 
 void buttonClick() {
   countINT++;
+  Serial.print("countINT: ");Serial.println(countINT);
 }
