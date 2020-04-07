9e48ce58b6aef4f3d1917fa43282f8750b984c45
nhamtung <nhamtung125@gmail.com>
2020-04-07 22:22:53 +0700

    save

--- Interrupt/Interrupt2/Interrupt2.ino
+++ Interrupt/Interrupt2/Interrupt2.ino
@@ -1,13 +1,13 @@
 /*
- * interruptModePin on Arduino Nano is pin D2 and D3
- * Connect button to GND pin and interruptModePin
+ * interruptModePin of Arduino Nano is pin D2 and pin D3
+ * Connect button to GND and interruptModePin
+ * 
  */
- 
+
 const byte interruptModePin = 3;
 const byte switchMode = 5;
 const byte ledPin = 13;
 const int timeWaitButtonMode = 500;
-
 int countINT = 0;
 volatile byte stateMode = HIGH;
 
@@ -26,8 +26,8 @@ void loop() {
     stateMode = !stateMode;
     Serial.println("Change Mode!");
   }
-  digitalWrite(switchMode, stateMode);
   digitalWrite(ledPin, stateMode);
+  digitalWrite(switchMode, stateMode);
   countINT = 0;
   delay(timeWaitButtonMode);
 }
