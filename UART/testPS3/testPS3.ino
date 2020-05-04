7b2b36ecf38bb18b2df864bb4871eb95b0fd772f
nhamtung <nhamtung125@gmail.com>
2020-05-04 22:44:07 +0700

    ad

--- UART/testPS3/testPS3.ino
+++ UART/testPS3/testPS3.ino
@@ -1,11 +1,16 @@
+char data_send[] = "TungNV send \n";
 
 void setup() {
   // initialize serial:
   Serial.begin(115200);
+  Serial.println("READY!");
 }
 
 void loop() 
-{}
+{
+  int bytesSent = Serial.write(data_send); //send the string and return the length of the string.
+  delay(1000);  
+}
 
 /*
   SerialEvent occurs whenever a new data comes in the
